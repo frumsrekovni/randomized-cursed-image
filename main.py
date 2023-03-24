4c03b157c74536ca8f2640d46559443257b1fd46
frumsrekovni <joshua.backstrom@gmail.com>
2023-03-25 00:07:27 +0100

    -you can now save your chicks

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import cv2 as cv
 
 #randomize the dimensions of the image
 def randomize_dimensions(img):
-    img = cv.resize(img, (random.randint(100, 1000), random.randint(100, 1000)))
+    img = cv.resize(img, (random.randint(40, 3000), random.randint(40, 3000)))
     return img
 #recolor the image
 def random_filter(img):
@@ -14,16 +14,32 @@ def random_filter(img):
                cv.COLOR_BGR2YCrCb, cv.COLOR_BGR2YUV]
     img = cv.cvtColor(img, random.choice(filters))
     return img
+
+
+#if I press the S key it saves the image to the folder as a png
+def save_image(img,filename):
+    filename = "chicks/" + filename + ".png"
+    cv.imwrite(filename, img)
+    print("Image",filename,"saved!")
+
+
+
+
+
 def main():
     while True:
         img = cv.imread("cc.jpg", cv.IMREAD_ANYCOLOR)
         img = randomize_dimensions(img)
         img = random_filter(img)
-        cv.imshow(str(''.join(random.choices(
-    string.ascii_letters, k=random.randint(30, 80)))), img)
-        if cv.waitKey(0) == 27:
+        filename = str(''.join(random.choices(
+    string.ascii_letters, k=random.randint(30, 80))))
+        cv.imshow(filename, img)
+        k = cv.waitKey(0)
+        if k == 27:
             cv.destroyAllWindows()
             sys.exit()
+        elif k == ord('s'):
+            save_image(img,filename)
         cv.destroyAllWindows()
 if __name__ == '__main__':
     main()
